ca809fc2b4141e4d27e6c0726441103fcee39afe
AG <arnabgupta91996@gmail.com>
2024-05-19 13:02:07 +0530

    adding rst test

--- docs/conf.py
+++ docs/conf.py
@@ -8,19 +8,19 @@ extensions = [
 
 latex_engine = 'xelatex'  # allow us to build Unicode chars
 
-html_static_path = ['_static']
-html_css_files = ['custom.css']
+# html_static_path = ['_static']
+# html_css_files = ['custom.css']
 
-html_theme_options = {
-    'style_nav_header_background': 'white',
-    # Toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False,
-    'dark_mode': True,  # Enable the dark mode toggle
-}
+# html_theme_options = {
+#     'style_nav_header_background': 'white',
+#     # Toc options
+#     'collapse_navigation': True,
+#     'sticky_navigation': True,
+#     'navigation_depth': 4,
+#     'includehidden': True,
+#     'titles_only': False,
+#     'dark_mode': True,  # Enable the dark mode toggle
+# }
 
 
 
